257ef6eba38f0fa86a4f6922ca1e0056a5e897c4
talitaoc <talitaoc@gmail.com>
2022-11-10 21:59:28 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+export const Banner = () =>{
+
+    //JSX como o react trabalha com a parte visual
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.jpg" alt="O banner principal do organo"/>
+        </header>
+    )
+
+}
